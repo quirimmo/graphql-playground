d0149bcd0cd7bce5e19cd5c88fb8e74d90f9fe5f
Quirino Lo Russo <quirimmo@gmail.com>
2018-12-19 00:52:51 +0100

    played with graphql

---
+++ client-mutations-and-input-types.js
@@ -0,0 +1,28 @@
+const fetch = require("node-fetch");
+
+var author = "andy";
+var content = "hope is a good thing";
+var query = `mutation CreateMessage($input: MessageInput) {
+  createMessage(input: $input) {
+    id
+  }
+}`;
+
+fetch("http://localhost:4000/graphql", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  },
+  body: JSON.stringify({
+    query,
+    variables: {
+      input: {
+        author,
+        content
+      }
+    }
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log("data returned:", data));
